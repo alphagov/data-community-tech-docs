9016052249f4554e82d6c01d6d4e7bd1ee9540e9
Ian Ansell <ian.ansell.uk@gmail.com>
2026-02-16 17:23:45 +0000

    remove publishing components

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,6 +1,1 @@
 //= require govuk_tech_docs
-//= require govuk_publishing_components/dependencies
-//= require govuk_publishing_components/lib/trigger-event
-//= require govuk_publishing_components/lib/cookie-settings
-//= require govuk_publishing_components/lib/cookie-functions
-//= require govuk_publishing_components/components/cookie-banner
